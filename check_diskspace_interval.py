2e9f3c90631f16563466711b8cfd4f65c674e7fd
p0x0q <p0x0q_jp@gmail.com>
2020-09-16 09:51:03 +0900

    Update check_diskspace_interval.py

--- check_diskspace_interval.py
+++ check_diskspace_interval.py
@@ -18,5 +18,5 @@ import time
 import datetime
 while True:
     dsk = psutil.disk_usage(args.drive)
-    print("{} : {}%".format(datetime.datetime.now(),dsk.percent))
-    time.sleep(1)
+    print("{}".format(datetime.datetime.now()),str(dsk.percent)+"%","Used:","{}GB".format(round(dsk.used/1024/1024/1024)),"Free:","{}GB".format(round(dsk.free/1024/1024/1024)),"Total:","{}GB".format(round(dsk.total/1024/1024/1024)))
+    time.sleep(10)
